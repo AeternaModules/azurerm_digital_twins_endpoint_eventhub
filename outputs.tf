380649ae8bfc93b3675155c01705a67a67cb1d6b
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:47:23 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "digital_twins_endpoint_eventhubs" {
+  description = "All digital_twins_endpoint_eventhub resources"
+  value       = azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs
+}
+
