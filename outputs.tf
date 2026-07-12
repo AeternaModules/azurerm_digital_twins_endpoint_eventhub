b21c7e7720a35968820a532c704f596b7777b814
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,24 @@
-output "digital_twins_endpoint_eventhubs" {
-  description = "All digital_twins_endpoint_eventhub resources"
-  value       = azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs
-  sensitive   = true
-}
 output "digital_twins_endpoint_eventhubs_dead_letter_storage_secret" {
-  description = "List of dead_letter_storage_secret values across all digital_twins_endpoint_eventhubs"
-  value       = [for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : v.dead_letter_storage_secret]
+  description = "Map of dead_letter_storage_secret values across all digital_twins_endpoint_eventhubs, keyed the same as var.digital_twins_endpoint_eventhubs"
+  value       = { for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : k => v.dead_letter_storage_secret }
   sensitive   = true
 }
 output "digital_twins_endpoint_eventhubs_digital_twins_id" {
-  description = "List of digital_twins_id values across all digital_twins_endpoint_eventhubs"
-  value       = [for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : v.digital_twins_id]
+  description = "Map of digital_twins_id values across all digital_twins_endpoint_eventhubs, keyed the same as var.digital_twins_endpoint_eventhubs"
+  value       = { for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : k => v.digital_twins_id }
 }
 output "digital_twins_endpoint_eventhubs_eventhub_primary_connection_string" {
-  description = "List of eventhub_primary_connection_string values across all digital_twins_endpoint_eventhubs"
-  value       = [for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : v.eventhub_primary_connection_string]
+  description = "Map of eventhub_primary_connection_string values across all digital_twins_endpoint_eventhubs, keyed the same as var.digital_twins_endpoint_eventhubs"
+  value       = { for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : k => v.eventhub_primary_connection_string }
   sensitive   = true
 }
 output "digital_twins_endpoint_eventhubs_eventhub_secondary_connection_string" {
-  description = "List of eventhub_secondary_connection_string values across all digital_twins_endpoint_eventhubs"
-  value       = [for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : v.eventhub_secondary_connection_string]
+  description = "Map of eventhub_secondary_connection_string values across all digital_twins_endpoint_eventhubs, keyed the same as var.digital_twins_endpoint_eventhubs"
+  value       = { for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : k => v.eventhub_secondary_connection_string }
   sensitive   = true
 }
 output "digital_twins_endpoint_eventhubs_name" {
-  description = "List of name values across all digital_twins_endpoint_eventhubs"
-  value       = [for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : v.name]
+  description = "Map of name values across all digital_twins_endpoint_eventhubs, keyed the same as var.digital_twins_endpoint_eventhubs"
+  value       = { for k, v in azurerm_digital_twins_endpoint_eventhub.digital_twins_endpoint_eventhubs : k => v.name }
 }
 
